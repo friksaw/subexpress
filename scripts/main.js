0576a1526694ca06ac2dfba55472d1d493f96b7d
freakssha <lestreng.begi@gmail.com>
2024-08-04 02:47:29 +0300

    add desc on hover benefit card

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,23 @@ function shiftCards() {
     container.innerHTML = '';
     cards.forEach(card => container.appendChild(card));
 }
+
+function openBenefitCard() {
+    document.getElementById("MyElement").classList.add('MyClass');
+
+    document.getElementById("MyElement").classList.remove('MyClass');
+
+    if ( document.getElementById("MyElement").classList.contains('MyClass') )
+
+        document.getElementById("MyElement").classList.toggle('MyClass');
+}
+
+const cards = document.querySelectorAll('.card-benefit');
+
+cards[1].classList.add('active')
+cards.forEach(card => {
+    card.addEventListener('mouseover', () => {
+        cards.forEach(c => c.classList.remove('active'));
+        card.classList.add('active')
+    });
+});
