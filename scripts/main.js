165f8409d825fc1c68782228e4efcb02da2c3044
freakssha <lestreng.begi@gmail.com>
2024-08-04 14:53:39 +0300

    fix slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,37 +1,16 @@
-const cards = document.querySelectorAll('.card-benefit');
-
-cards[1].classList.add('active')
-cards.forEach(card => {
-    card.addEventListener('mouseover', () => {
-        cards.forEach(c => c.classList.remove('active'));
-        card.classList.add('active')
-    });
-});
-
-function sendEmail()  {
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
-
-    Email.send("Zakaz@subexpress.ru",
-        "Zakaz@subexpress.ru",
-        "This is a subject",
-        name + email + phone,
-        {token: "63cb3a19-2684-44fa-b76f-debf422d8b00"});
-}
 
 function sendMessage() {
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
 
-    const subject = 'Website Change Request';
+    const subject = `New request from ${name}`;
 
     const data = {
-        to: 'Zakaz@subexpress.ru',
+        to: 'dani.dimitrievna@gmail.com',
         subject: subject,
         from: email,
-        cc: 'susan@example.com',
+        cc: '',
         message: name + email + phone
     };
 
@@ -49,13 +28,22 @@ function sendMessage() {
             return response.text();
         })
         .then(data => {
-            // Успешная отправка
             alert('Письмо отправлено!');
         })
         .catch(error => {
             console.error('Ошибка:', error);
         });
 }
+
+const cards = document.querySelectorAll('.card-benefit');
+
+cards[1].classList.add('active')
+cards.forEach(card => {
+    card.addEventListener('mouseover', () => {
+        cards.forEach(c => c.classList.remove('active'));
+        card.classList.add('active')
+    });
+});
 
 document.addEventListener("DOMContentLoaded", function() {
     const carousel = document.querySelector(".carousel");
@@ -102,5 +90,38 @@ document.addEventListener("DOMContentLoaded", function() {
         carousel.classList.remove("dragging");
     };
 
-})
+    const autoPlay = () => {
 
+        // Return if window is smaller than 800
+        if (window.innerWidth < 800) return;
+
+        // Calculate the total width of all cards
+        const totalCardWidth = carousel.scrollWidth;
+
+        // Calculate the maximum scroll position
+        const maxScrollLeft = totalCardWidth - carousel.offsetWidth;
+
+        // If the carousel is at the end, stop autoplay
+        if (carousel.scrollLeft >= maxScrollLeft) return;
+
+        // Autoplay the carousel after every 2500ms
+        timeoutId = setTimeout(() =>
+            carousel.scrollLeft += firstCardWidth, 2500);
+    };
+
+    carousel.addEventListener("mousedown", dragStart);
+    carousel.addEventListener("mousemove", dragging);
+    document.addEventListener("mouseup", dragStop);
+    wrapper.addEventListener("mouseenter", () =>
+        clearTimeout(timeoutId));
+    wrapper.addEventListener("mouseleave", autoPlay);
+
+    // Add event listeners for the arrow buttons to
+    // scroll the carousel left and right
+    arrowBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            carousel.scrollLeft += btn.id === "left" ?
+                -firstCardWidth : firstCardWidth;
+        });
+    });
+});
