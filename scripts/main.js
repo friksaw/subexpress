b849120e6732f894eceaa782a49d51c002f9072e
freakssha <lestreng.begi@gmail.com>
2024-08-04 12:14:34 +0300

    add stars slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,3 @@
-function shiftCards() {
-    const container = document.getElementById('container-cr');
-    const cards = Array.from(container.children); // Преобразуем HTMLCollection в массив
-
-    // "Сдвигаем" элементы массива
-    cards.push(cards.shift());
-
-    // Очищаем контейнер и добавляем карточки в новом порядке
-    container.innerHTML = '';
-    cards.forEach(card => container.appendChild(card));
-}
-
-function openBenefitCard() {
-    document.getElementById("MyElement").classList.add('MyClass');
-
-    document.getElementById("MyElement").classList.remove('MyClass');
-
-    if ( document.getElementById("MyElement").classList.contains('MyClass') )
-
-        document.getElementById("MyElement").classList.toggle('MyClass');
-}
-
 const cards = document.querySelectorAll('.card-benefit');
 
 cards[1].classList.add('active')
@@ -27,5 +5,86 @@ cards.forEach(card => {
     card.addEventListener('mouseover', () => {
         cards.forEach(c => c.classList.remove('active'));
         card.classList.add('active')
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const carousel = document.querySelector(".carousel");
+    const arrowBtns = document.querySelectorAll(".wrapper i");
+    const wrapper = document.querySelector(".wrapper");
+
+    const firstCard = carousel.querySelector(".card");
+    const firstCardWidth = firstCard.offsetWidth;
+
+    let isDragging = false,
+        startX,
+        startScrollLeft,
+        timeoutId;
+
+    const dragStart = (e) => {
+        isDragging = true;
+        carousel.classList.add("dragging");
+        startX = e.pageX;
+        startScrollLeft = carousel.scrollLeft;
+    };
+
+    const dragging = (e) => {
+        if (!isDragging) return;
+
+        // Calculate the new scroll position
+        const newScrollLeft = startScrollLeft - (e.pageX - startX);
+
+        // Check if the new scroll position exceeds
+        // the carousel boundaries
+        if (newScrollLeft <= 0 || newScrollLeft >=
+            carousel.scrollWidth - carousel.offsetWidth) {
+
+            // If so, prevent further dragging
+            isDragging = false;
+            return;
+        }
+
+        // Otherwise, update the scroll position of the carousel
+        carousel.scrollLeft = newScrollLeft;
+    };
+
+    const dragStop = () => {
+        isDragging = false;
+        carousel.classList.remove("dragging");
+    };
+
+    const autoPlay = () => {
+
+        // Return if window is smaller than 800
+        if (window.innerWidth < 800) return;
+
+        // Calculate the total width of all cards
+        const totalCardWidth = carousel.scrollWidth;
+
+        // Calculate the maximum scroll position
+        const maxScrollLeft = totalCardWidth - carousel.offsetWidth;
+
+        // If the carousel is at the end, stop autoplay
+        if (carousel.scrollLeft >= maxScrollLeft) return;
+
+        // Autoplay the carousel after every 2500ms
+        timeoutId = setTimeout(() =>
+            carousel.scrollLeft += firstCardWidth, 2500);
+    };
+
+    carousel.addEventListener("mousedown", dragStart);
+    carousel.addEventListener("mousemove", dragging);
+    document.addEventListener("mouseup", dragStop);
+    wrapper.addEventListener("mouseenter", () =>
+        clearTimeout(timeoutId));
+    wrapper.addEventListener("mouseleave", autoPlay);
+
+    // Add event listeners for the arrow buttons to
+    // scroll the carousel left and right
+    arrowBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            carousel.scrollLeft += btn.id === "left" ?
+                -firstCardWidth : firstCardWidth;
+        });
     });
 });
